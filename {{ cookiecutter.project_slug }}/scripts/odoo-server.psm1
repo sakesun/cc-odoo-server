3ac1d7648351f8c74f2c54723d3e87b7e8511775
sake <sakesun@gmail.com>
2022-08-30 11:47:56 +0700

    now work

--- {{ cookiecutter.project_slug }}/scripts/odoo-server.psm1
+++ {{ cookiecutter.project_slug }}/scripts/odoo-server.psm1
@@ -3,88 +3,240 @@ $PATH_ROOT      = Join-Path $PSScriptRoot ".."
 $PATH_VENV      = Join-Path $PATH_ROOT "venv"
 $PATH_ODOO      = Join-Path $PATH_ROOT "odoo"
 $PATH_ADDONS    = Join-Path $PATH_ROOT "addons"
-$CONFIG_FILE    = Join-Path $PATH_ROOT "odoo-server-config.json"
-
-function Get-DefaultBranch($targetName) {
-    return $DEFAULT_BRANCH
-}
+$CONFIG_FILE    = Join-Path $PATH_ROOT "{{ cookiecutter.project_slug }}-config.json"
 
 function localGitSource($path) {
     return "file://$($path.Replace('\', '/'))"
 }
 
 function checkConfigFile {
-    $configExists = Test-Path $CONFIG_FILE -PathType Leaf
-    if (! $configExists) {
+    $configExists = Test-Path -PathType Leaf $CONFIG_FILE
+    if (-Not $configExists) {
         $branch = $DEFAULT_BRANCH
         $defaultContent = (
             ConvertTo-Json -Depth 100 (
                 [ordered]@{
                     "odoo" = [ordered]@{
-                        "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "odoo");
+                        "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "odoo")
                         "branch" = $branch
                     };
                     "addons" = [ordered]@{
                         "enterprise" = @{
-                            "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "enterprise");
-                            "branch" = $branch;
-                            "dirs"   = @(".");
+                            "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "enterprise")
+                            "branch" = $branch
+                            "dirs"   = @(".")
                         };
                         "design-themes" = [ordered]@{
-                            "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "design-themes");
-                            "branch" = $branch;
-                            "dirs"   = @(".");
+                            "source" = localGitSource (Join-Path $PSScriptRoot ".." ".." "odoo-src" "design-themes")
+                            "branch" = $branch
+                            "dirs"   = @(".")
                         }
                     };
                     "db" = [ordered]@{
-                        "server" = "127.0.0.1";
+                        "server" = "127.0.0.1"
                         "port"   = 5432
-                        "root"   = "postgres";
-                        "name"   = "odoo";
-                        "user"   = "user";
-                        "pass"   = "password"
-                    };
+                        "root"   = "postgres"
+                        "name"   = "{{ cookiecutter.db_name }}"
+                        "user"   = "{{ cookiecutter.db_user }}"
+                        "pass"   = "{{ cookiecutter.db_pass }}"
+                    }
                     "server" = [ordered]@{
-                        "http-port"        = 8069;
-                        "longpolling-port" = 8072;
+                        "http-port"        = 8069
+                        "longpolling-port" = 8072
                     }
                 }
             )
         )
-        [Console]::Error.WriteLine("Need $CONFIG_FILE")
-        [Console]::Error.WriteLine("`nGenerate default content:`n")
-        [Console]::Error.WriteLine("$defaultContent")
-        $defaultContent | Out-File $CONFIG_FILE
+        $host.ui.WriteErrorLine(
+            @(
+                "Need $CONFIG_FILE"
+                "`nSample:`n"
+                "$defaultContent"
+                ""
+            ) -Join "`n"
+        )
+        throw "Need config file"
     }
-    return $configExists
 }
 
 function loadConfig {
+    checkConfigFile
     return (Get-Content -Path $CONFIG_FILE | ConvertFrom-Json -AsHashtable)
 }
 
-function checkOut($source, $branch, $target) {
-    if (Test-Path $target -PathType Container) { return }
-    if ($branch -eq $null) {
-        $branch = Get-DefaultBranch -targetName (Split-Path -Leaf $target)
+function buildConnectionString {
+    param (
+        [hashtable] $params
+    )
+    $lines = @()
+    foreach ($p in $params.GetEnumerator()) {
+        $lines += "$($p.Name)=$($p.Value)"
     }
-    git clone --depth 1 --branch $branch $source $target
+    return ($lines -Join ";")
 }
 
-function Initialize-Sources() {
-    if (! (checkConfigFile)) { return }
-    $config = loadConfig
+$typeAdded = $false
+
+function pgQuery($server, $port, $dbname, $user, $pass, $sql) {
+    if (-Not $typeAdded) {
+        Add-Type -Path (Join-Path $PSScriptRoot "Npgsql.dll")
+        $typeAdded = $true
+    }
+    $params = @{
+        "Server"   = $server;
+        "Port"     = $port;
+        "User Id"  = $user;
+    }
+    if ($dbname -ne $null) { $params.Database = $dbname; }
+    if ($pass -ne $null) { $params.Password = $pass; }
+    if ($params.Server -eq $null) { $params.Server = "127.0.0.1" }
+    if ($params.Port   -eq $null) { $params.Port   = 5432 }
+    $connectionString = buildConnectionString $params
+    $conn = [Npgsql.NpgsqlConnection]::new($connectionString)
+    [void] $conn.Open()
+    try {
+        $cmd = $conn.CreateCommand()
+        $cmd.CommandText = $sql
+        $reader = $cmd.ExecuteReader()
+        try {
+            $table = [System.Data.DataTable]::new()
+            $table.Load($reader)
+            return $table
+        } finally {
+            [void] $reader.Close()
+        }
+    } finally {
+        [void] $conn.Close()
+    }
+}
+
+function query($config, $sql) {
+    return pgQuery `
+      -server $config.db.server `
+      -port   $config.db.port `
+      -dbname $config.db.name `
+      -user   $config.db.user `
+      -pass   $config.db.pass `
+      -sql    $sql
+}
+
+function queryWithRoot($config, $sql) {
+    return pgQuery `
+      -server $config.db.server `
+      -port   $config.db.port `
+      -user   $config.db.root `
+      -sql    $sql
+}
+
+function databaseExists($config) {
+    $sql = "select datname from pg_catalog.pg_database where datname = '$($config.db.name)'"
+    $t = queryWithRoot $config $sql
+    return ($t.Rows.Count -ge 1)
+}
+
+function userExists($config) {
+    $sql = "select usename from pg_catalog.pg_user where usename = '$($config.db.user)'"
+    $t = queryWithRoot $config $sql
+    return ($t.Rows.Count -ge 1)
+}
+
+function tableExists($config, $table) {
+    $sql = @(
+        "select tablename from pg_catalog.pg_tables"
+        "  where schemaname = 'public'"
+        "    and tablename  = '$table'"
+    ) -Join "`n"
+    $t = queryWithRoot $config $sql
+    return ($t.Rows.Count -ge 1)
+}
+
+function moduleExists($config, $module) {
+    $sql = "select name from ir_module_module where name = '$module'"
+    $t = query $config $sql
+    return ($t.Rows.Count -ge 1)
+}
+
+function dropDatabaseAndUser($config) {
+    psql -U "$($config.db.root)" -c "drop database if exists $($config.db.name)"
+    psql -U "$($config.db.root)" -c "drop user     if exists $($config.db.user)"
+}
+
+function createDatabaseAndUser($config) {
+    if (-Not (databaseExists $config)) {
+        psql -U "$($config.db.root)" -c "create database $($config.db.name)"
+    }
+    if (-Not (userExists $config)) {
+        psql -U "$($config.db.root)" -c "create user $($config.db.user) with password '$($config.db.pass)'"
+    }
+    psql -U "$($config.db.root)" -c "alter database $($config.db.name) owner to $($config.db.user)"
+}
+
+function Remove-OdooDatabaseAndUser {
+    param(
+        [switch] $Force
+    )
+    if (-Not $Force) { throw "Need -Force do remove Odoo database" }
+    dropDatabaseAndUser $(loadConfig)
+}
+
+function checkOut($source, $branch, $target) {
+    if (Test-Path -PathType Container $target) { return }
+    if ($branch -eq $null) {
+        git clone --depth 1                  $source $target
+    } else {
+        git clone --depth 1 --branch $branch $source $target
+    }
+}
+
+function initializeSources($config) {
     if ($config.odoo -ne $null) {
-        checkOut $config.odoo.source $config.odoo.branch $PATH_ODOO
+        checkOut `
+            -source $config.odoo.source `
+            -branch $config.odoo.branch `
+            -target $PATH_ODOO
     }
     foreach ($addon in $config.addons.GetEnumerator()) {
-        [void](New-Item -Force -ItemType Directory $PATH_ADDONS)
-        checkOut $addon.Value.source $addon.Value.branch (Join-Path $PATH_ADDONS $addon.Name)
+        [void] (New-Item -Force -ItemType Directory $PATH_ADDONS)
+        checkOut `
+            -source $addon.Value.source `
+            -branch $addon.Value.branch `
+            -target (Join-Path $PATH_ADDONS $addon.Name)
     }
+}
+
+function removeIfExists($target) {
+    if (-Not (Test-Path -PathType Container $target)) { return }
+    if (-not (($target -eq $null) -or ($target -eq ""))) {
+        Remove-Item -r -force $target
+    }
+}
+
+function initializeVenv {
+    if (Test-Path -PathType Container $PATH_VENV) { return }
+    pyenv exec python -m venv "$PATH_VENV"
+    . $PATH_VENV/Scripts/activate.ps1
+    python -m ensurepip   --upgrade
+    python -m pip install --upgrade pip
+    python -m pip install -e "$PATH_ODOO"
+    python -m pip install -r "$PATH_ODOO/requirements.txt"
+    python -m pip install    psycopg2-binary   # psocopg2 does not work on Windows
+    python -m pip install    pdfminer.six
+    python -m pip install    ipython
+    python -m pip install    ipdb
+    python -m pip install    watchdog
+    python -m pip install    odoorpc
+
+    # for enterprise
+    python -m pip install    dbfread
+    python -m pip install    google_auth
+    python -m pip install    dbfread
+    python -m pip install    dbfread
+    python -m pip install    dbfread
+    python -m pip install    phonenumbers
 }
 
 function isValidAddonPath($p) {
-    if (! (Test-Path $p)) { return $false }
+    if (-Not (Test-Path $p)) { return $false }
     $countDir = Get-ChildItem -Directory $p | Measure-Object | Select-Object -ExpandProperty Count
     return ($countDir -gt 0)
 }
@@ -102,16 +254,15 @@ function addIfValidAddon {
     )
     ForEach ($p in $paths) {
         if (isValidAddonPath $p) {
-            [void]$list.Add((Get-Item $p).FullName)
+            [void] $list.Add((Get-Item $p).FullName)
         }
     }
 }
 
-function Get-AllAddonPaths {
+function getAllAddonPaths($config) {
     $addons = [System.Collections.ArrayList]@()
     addIfValidAddon $addons "$PATH_ODOO/addons"
-    $config = loadConfig
-    foreach ($addon in (Get-ChildItem -Directory $PATH_ADDONS)) {
+    foreach ($addon in (Get-ChildItem $PATH_ADDONS)) {
         $dirs = $null
         if ($config.addons -ne $null) { $dirs = $config.addons[$addon.Name].dirs }
         if ($dirs -eq $null) { $dirs = @(".") }
@@ -124,10 +275,18 @@ function Get-AllAddonPaths {
 }
 
 function Get-AllAddons {
-    return (Get-AllAddonPaths
+    return (getAllAddonPaths $(loadConfig)
             | Get-ChildItem -Directory
             | Where-Object { isValidAddonModulePath $_ }
             | Select-Object -ExpandProperty Name )
+}
+
+function getInstalledAddons($config){
+    return query $config "select * from ir_module_module where state = 'installed'"
+}
+
+function Get-InstalledAddons {
+    return (getInstalledAddons $(loadConfig) | Select-Object -ExpandProperty name)
 }
 
 function Get-InstallableAddons {
@@ -153,79 +312,9 @@ function Get-InstallableAddons {
         $false
     }
     $addons = ($excluded + $included) | sort | Get-Unique
-    $installed = [System.Collections.Generic.HashSet[String]]@(Get-InstalledAddons | Select -ExpandProperty Name)
-    $addons = $addons | ? {! $installed.Contains($_)}
+    $installed = [System.Collections.Generic.HashSet[String]]@(Get-InstalledAddons)
+    $addons = $addons | ? {-Not $installed.Contains($_)}
     return $addons
-}
-
-function Build-ConnectionString {
-    param (
-        [hashtable] $params
-    )
-    $lines = @()
-    foreach ($p in $params.GetEnumerator()) {
-        $lines += "$($p.Name)=$($p.Value)"
-    }
-    return [String]::Join(";", $lines)
-}
-
-function Read-OdooDatabase ($sql) {
-    $config = loadConfig
-    Add-Type -Path (Join-Path $PSScriptRoot "Npgsql.dll")
-    $params = @{
-        "Server"   = $config.db.server;
-        "Port"     = $config.db.port;
-        "User Id"  = $config.db.user;
-        "Password" = $config.db.pass;
-    }
-    if ($params.Server -eq $null) { $params.Server = "127.0.0.1" }
-    if ($params.Port   -eq $null) { $params.Port   = 5432 }
-    $connectionString = Build-ConnectionString $params
-    $conn = [Npgsql.NpgsqlConnection]::new($connectionString)
-    [void] $conn.Open()
-    try {
-        $cmd = $conn.CreateCommand()
-        $cmd.CommandText = $sql
-        $reader = $cmd.ExecuteReader()
-        try {
-            $table = [System.Data.DataTable]::new()
-            $table.Load($reader)
-            return $table
-        } finally {
-            [void] $reader.Close()
-        }
-        return $conn.Settings
-    } finally {
-        [void] $conn.Close()
-    }
-}
-
-function Get-InstalledAddons {
-    return Read-OdooDatabase "select * from ir_module_module imm where state = 'installed'"
-}
-
-function Initialize-Venv {
-    if (! (checkConfigFile)) { return }
-    pyenv exec python -m venv "$PATH_VENV"
-    . $PATH_VENV/Scripts/activate.ps1
-    python -m ensurepip   --upgrade
-    python -m pip install --upgrade pip
-    python -m pip install -e "$PATH_ODOO"
-    python -m pip install -r "$PATH_ODOO/requirements.txt"
-    python -m pip install    psycopg2-binary   # psocopg2 does not work on Windows
-    python -m pip install    pdfminer.six
-    python -m pip install    ipython
-    python -m pip install    ipdb
-    python -m pip install    watchdog
-    python -m pip install    odoorpc
-
-    # for enterprise
-    python -m pip install    dbfread
-    python -m pip install    google_auth
-    python -m pip install    dbfread
-    python -m pip install    dbfread
-    python -m pip install    dbfread
-    python -m pip install    phonenumbers
 }
 
 function Invoke-OdooBin {
@@ -249,7 +338,7 @@ function Invoke-OdooBin {
     $python      = (Resolve-Path "$PATH_VENV/Scripts/python.exe").Path
     $odoo_bin    = (Resolve-Path "$PATH_ODOO/odoo-bin").Path
     $gevent_arg  = $gevent ? "gevent" : ""
-    $all_addons  = Get-AllAddonPaths
+    $all_addons  = getAllAddonPaths
     if (${Addons}.Length -gt 0) {
         $all_addons = $all_addons + $Addons
     }
@@ -259,7 +348,7 @@ function Invoke-OdooBin {
     }
     $arguments += "--addons-path=`"$($all_addons -join ',')`""
     $arguments += $remaining
-    $watching_paths = Get-AllAddonPaths | Get-ChildItem | ? { $_.Name -in $watch }
+    $watching_paths = getAllAddonPaths | Get-ChildItem | ? { $_.Name -in $watch }
     if ($watching_paths.Length -gt 0) {
         if ($ext.Length -eq 0) {
             $ext = @("py"; "csv"; "xml"; "xls"; "xlsx"; "po"; "rst"; "html"; "css"; "js"; "ts"; "png"; "svg"; "jpg"; "ico")
@@ -273,7 +362,7 @@ function Invoke-OdooBin {
         $exec = (@($python; $odoo_bin; $arguments) -join ' ')
         $nodemon_arguments = @( "-x"; "$($exec)";
                                 "-e"; "$($ext -join ' ')" )
-        ForEach($w in $watching_paths) {
+        foreach ($w in $watching_paths) {
             $nodemon_arguments += @("-w"; ('"' + $w.FullName + '"'))
         }
         $command = 'nodemon'
@@ -308,86 +397,82 @@ function Test-Odoo {
     }
     #
     Invoke-OdooBin -- `
-      -u $targets `
-      --test-enable `
-      --log-level=test `
-      --stop-after-init `
-      @remaining
+        -u $targets `
+        --test-enable `
+        --log-level=test `
+        --stop-after-init `
+        @remaining
 }
 
-function Initialize-Config {
-    if (! (checkConfigFile)) { return }
-    $config = loadConfig
+function initializeBaseAndSaveConfig($config) {
     $arguments = @()
     if ($config.server -ne $null) {
         $arguments += "--http-port=$($config.server['http-port'])"
         $arguments += "--longpolling-port=$($config.server['longpolling-port'])"
     }
-    Invoke-OdooBin -- `
-      --stop-after-init `
-      -d $config.db.name `
-      -r $config.db.user `
-      -w $config.db.pass `
-      -i base `
-      @argumenst
-    Invoke-OdooBin -- `
-      --stop-after-init `
-      -d $config.db.name `
-      -r $config.db.user `
-      -w $config.db.pass `
-      --save `
-      @arguments
-}
-
-function Initialize-Everything {
-    if (! (checkConfigFile)) { return }
-    Initialize-Sources
-    Initialize-Venv
-    Initialize-Config
-}
-
-function Remove-RecursiveForce($target) {
-    if (-not (($target -eq $null) -or ($target -eq ""))) {
-        rm -r -force $target
+    $baseInstalled = (tableExists $config "ir_module_module") -And (moduleExists $config "base")
+    if (-Not $baseInstalled) {
+        Invoke-OdooBin -- `
+          --stop-after-init `
+          -d $config.db.name `
+          -r $config.db.user `
+          -w $config.db.pass `
+          -i base `
+          @argumenst
     }
+    Invoke-OdooBin -- `
+        --stop-after-init `
+        -d $config.db.name `
+        -r $config.db.user `
+        -w $config.db.pass `
+        --save `
+        @arguments
 }
 
-function Reset-Everything {
-    if (! (checkConfigFile)) { return }
-    Remove-RecursiveForce $PATH_VENV
-    Remove-RecursiveForce $PATH_ODOO
-    Remove-RecursiveForce $PATH_ADDONS
-    $config = loadConfig
-    psql -U $config.db.root -c "drop   database $($config.db.name)"
-    psql -U $config.db.root -c "create database $($config.db.name)"
-    psql -U $config.db.root -c "alter  database $($config.db.name) owner to $($config.db.user)"
-}
-
-function Install-AllAddons {
+function Install-AllInstallableAddons {
     $addons = Get-InstallableAddons
     if ($addons.count -gt 0) {
+        $installing = ($addons -join ',')
+        Write-Output "Installing:`n  $installing"
         Invoke-OdooBin -- --stop-after-init -i "$($addons -join ',')"
     } else {
         Write-Output "All addons is already installed"
     }
 }
 
+function Initialize-OdooServer {
+    param (
+        [switch] $Reinitialize
+    )
+    $config = loadConfig
+
+    # sources
+    if ($Reinitialize) {
+        removeIfExists $PATH_ODOO
+        removeIfExists $PATH_ADDONS
+    }
+    initializeSources $config
+
+    # venv
+    if ($Reinitialize) {
+        removeIfExists $PATH_VENV
+    }
+    initializeVenv
+
+    # db
+    if ($Reinitialize) { dropDatabaseAndUser $config }
+    createDatabaseAndUser $config
+
+    initializeBaseAndSaveConfig $config
+}
+
 Export-ModuleMember `
   -Function @(
-      "Get-DefaultBranch",
-      "Invoke-OdooBin",
-      "Test-Odoo",
-      "Reset-Everything",
-      "Initialize-Sources",
-      "Initialize-Venv",
-      "Initialize-Config",
-      "Initialize-Everything",
-      "Get-AllAddonPaths",
-      "Get-AllAddons",
-      "Get-InstalledAddons",
-      "Install-AllAddons",
-      "Build-ConnectionString",
-      "Read-OdooDatabase",
-      "Get-InstallableAddons",
+      "Remove-OdooDatabaseAndUser"
+      "Initialize-OdooServer"
+      "Get-AllAddons"
       "Get-InstalledAddons"
+      "Get-InstallableAddons"
+      "Install-AllInstallableAddons"
+      "Invoke-OdooBin"
   )
