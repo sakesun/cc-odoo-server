e30ed8c74722db7f72f8809f425b65180098a558
sake <sakesun@gmail.com>
2022-09-07 12:19:30 +0700

    prefer unix style path

--- {{ cookiecutter.project_slug }}/activate.ps1
+++ {{ cookiecutter.project_slug }}/activate.ps1
@@ -1,5 +1,5 @@
-$venvActivate = "$PSScriptRoot\venv\Scripts\Activate.ps1"
+$venvActivate = "$PSScriptRoot/venv/Scripts/Activate.ps1"
 if (Test-Path $venvActivate) {
     . $venvActivate
 }
-import-module  $PSScriptRoot\scripts\odoo-server.psm1 -Force
+import-module  $PSScriptRoot/scripts/odoo-server.psm1 -Force
