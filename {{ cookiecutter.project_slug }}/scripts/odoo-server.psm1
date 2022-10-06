4d98b34a6ef98ae481db1003ae1569ce7eccc32c
sake <sakesun@gmail.com>
2022-10-06 16:37:55 +0700

    remove resolveCryptographyFailure, clone with depth 20

--- {{ cookiecutter.project_slug }}/scripts/odoo-server.psm1
+++ {{ cookiecutter.project_slug }}/scripts/odoo-server.psm1
@@ -209,9 +209,9 @@ function Remove-OdooDatabaseAndUser {
 function checkOut($source, $branch, $target) {
     if (Test-Path -PathType Container $target) { return }
     if ($branch -eq $null) {
-        git clone --depth 1                  $source $target
+        git clone --depth 20                  $source $target
     } else {
-        git clone --depth 1 --branch $branch $source $target
+        git clone --depth 20 --branch $branch $source $target
     }
 }
 
@@ -317,7 +317,7 @@ function initializeVenv {
     python -m pip install    odoorpc
 
     revertToWerkzeug1
-    resolveCryptographyFailure   # required for OCA/OCB
+    # resolveCryptographyFailure
 
     $config = (loadConfig)
     foreach ($addon in (Get-ChildItem $PATH_ADDONS)) {
