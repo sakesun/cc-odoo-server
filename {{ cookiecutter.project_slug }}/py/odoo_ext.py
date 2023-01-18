da00d1b8532dfc2e8f7ba08c3f001139fcbb8ff6
sake <sakesun@gmail.com>
2023-01-18 20:34:49 +0700

    fix defect

--- {{ cookiecutter.project_slug }}/py/odoo_ext.py
+++ {{ cookiecutter.project_slug }}/py/odoo_ext.py
@@ -6,9 +6,15 @@ def odoo_ref(*path):
     THIS_PATH = dirname(realpath(odoo.__file__))
     return join(THIS_PATH, '..', *path)
 
+loaded = False
+
 def load_config(filename=None):
+    global loaded
+    if loaded: raise Exception('Config is already loaded.')
+    from odoo.tools import config
     if filename is None: filename = odoo_ref('odoo.conf')
     config.parse_config(['-c', filename])
+    loaded = True
 
 def _create_env(uid, context):
     from odoo import registry
@@ -18,11 +24,13 @@ def _create_env(uid, context):
     return Environment(cr, uid, context)
 
 def create_super_env(context=None):
+    if not loaded: raise Exception('Config is not loaded yet.')
     from odoo import SUPERUSER_ID
     if context is None: context = {}
     return _create_env(SUPERUSER_ID, context)
 
 def create_env(uid=None, context=None):
+    if not loaded: raise Exception('Config is not loaded yet.')
     if uid is None:
         return create_super_env(context)
     else:
